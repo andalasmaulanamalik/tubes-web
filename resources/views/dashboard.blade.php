13e561cb49abed87cc06a71d36f7927ddfd23483
andalasmaulanamalik <andalas205@gmail.com>
2025-01-07 08:20:39 +0700

    perbaikan dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,32 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-center">
             {{ __('SELAMAT DATANG 😃') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @if(auth()->user() && auth()->user()->role === 'admin')
-                    <x-table name="header">
-                        <tr class="py-10">
-                            <th scope="col">#</th>
-                            <th scope="col">pengguna aktif</th>
-                            <th scope="col">total transaksi</th>
-                            <th scope="col">pendapatan harian</th>
-                            <th scope="col">pendapatan mingguan</th>
-                            <th scope="col">pendapatan bulanan</th>
-                            <th scope="col">&nbsp;</th>
-                        </tr>
-                    </x-table>
-                    @endif
-                    @if(auth()->user() && auth()->user()->role === 'user')
-                    
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
+    
 </x-app-layout>
